51e22d2f7d1317a2cf266d4c8c806751361e88ed
LittleBeannie <yujie.zhao@merck.com>
2022-08-19 09:23:54 -0400

    check S3 generic/method consistency

--- R/as_gt.R
+++ R/as_gt.R
@@ -14,6 +14,15 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#' S3 class method to get summary table into a gt table
+#'
+#' @param x a summary of fixed or group sequential design
+#' @param ... additional arguments
+#'
+#' @return a gt table
+#' @noRd
+#'
 as_gt <- function(x, ...) {
   UseMethod("as_gt", x)
 }
--- R/summary.R
+++ R/summary.R
@@ -15,6 +15,14 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+#' S3 class method to summary fixed or group sequential design
+#'
+#' @param x a fixed design object or a group sequential design object
+#' @param ... additional arguments
+#'
+#' @return a R data frame
+#' @noRd
+#' 
 summary <- function(x, ...) {
   UseMethod("summary", x)
 }
